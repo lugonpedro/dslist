9d77dc83d210bcd12b657411535efe2fdaecdd3f
Pedro Lugon <pedromotta20@gmail.com>
2024-08-22 21:02:33 -0300

    feat(game): add request and response dto

---
+++ src/main/java/com/devsuperior/dslit/dto/GameRequestDTO.java
@@ -0,0 +1,86 @@
+package com.devsuperior.dslit.dto;
+
+import com.devsuperior.dslit.entities.Game;
+import org.springframework.beans.BeanUtils;
+
+public class GameRequestDTO {
+    private Long id;
+    private String title;
+    private Integer year;
+    private String genre;
+    private String platforms;
+    private String imgUrl;
+    private Double score;
+    private String description;
+
+    public GameRequestDTO(){
+    }
+
+    public GameRequestDTO(Game entity){
+        BeanUtils.copyProperties(entity, this);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- src/main/java/com/devsuperior/dslit/dto/GameResponseDTO.java
+++ src/main/java/com/devsuperior/dslit/dto/GameResponseDTO.java
@@ -2,17 +2,17 @@ package com.devsuperior.dslit.dto;
 
 import com.devsuperior.dslit.entities.Game;
 
-public class GameDTO {
+public class GameResponseDTO {
     private Long id;
     private String title;
     private Integer year;
     private String imgUrl;
     private String description;
 
-    public GameDTO(){
+    public GameResponseDTO(){
     }
 
-    public GameDTO(Game entity){
+    public GameResponseDTO(Game entity){
         id = entity.getId();
         title = entity.getTitle();
         year = entity.getYear();
