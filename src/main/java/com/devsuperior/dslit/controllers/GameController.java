355421201a92e3752c061139bd5d6f0cbcb42090
Pedro Lugon <pedromotta20@gmail.com>
2024-08-22 21:02:40 -0300

    feat(game): add find by id

--- src/main/java/com/devsuperior/dslit/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslit/controllers/GameController.java
@@ -1,10 +1,10 @@
 package com.devsuperior.dslit.controllers;
 
-import com.devsuperior.dslit.dto.GameDTO;
-import com.devsuperior.dslit.entities.Game;
+import com.devsuperior.dslit.dto.GameResponseDTO;
 import com.devsuperior.dslit.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,17 @@ public class GameController {
     @Autowired
     private GameService service;
 
+//    public GameDTO create() {
+//        this.service.
+//    }
+
+    @GetMapping(value = "/{id}")
+    public GameResponseDTO findById(@PathVariable Long id) {
+        return this.service.findById(id);
+    }
+
     @GetMapping
-    public List<GameDTO> findAll() {
+    public List<GameResponseDTO> findAll() {
         return this.service.findAll();
     }
 }
--- src/main/java/com/devsuperior/dslit/services/GameService.java
+++ src/main/java/com/devsuperior/dslit/services/GameService.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dslit.services;
 
-import com.devsuperior.dslit.dto.GameDTO;
+import com.devsuperior.dslit.dto.GameRequestDTO;
+import com.devsuperior.dslit.dto.GameResponseDTO;
 import com.devsuperior.dslit.entities.Game;
 import com.devsuperior.dslit.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,8 +15,19 @@ public class GameService {
     @Autowired
     private GameRepository repository;
 
-    public List<GameDTO> findAll() {
+//    public GameDTO create(GameRequestDTO body) {
+//        Game game = new Game(body);
+//        repository.save(game);
+//        return Game;
+//    }
+
+    public GameResponseDTO findById(Long id) {
+        Game res = repository.findById(id).orElseThrow(() -> new RuntimeException("Game not found"));
+        return new GameResponseDTO(res);
+    }
+
+    public List<GameResponseDTO> findAll() {
         List<Game> res = repository.findAll();
-        return res.stream().map(GameDTO::new).toList();
+        return res.stream().map(GameResponseDTO::new).toList();
     }
 }
