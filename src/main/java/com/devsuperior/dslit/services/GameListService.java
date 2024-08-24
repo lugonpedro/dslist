5f23a55b8a1fc00d6f23ee04ad24761594446d3f
Pedro Lugon <pedromotta20@gmail.com>
2024-08-24 02:39:29 -0300

    feat(game list): add game move inside list

--- src/main/java/com/devsuperior/dslit/services/GameListService.java
+++ src/main/java/com/devsuperior/dslit/services/GameListService.java
@@ -3,9 +3,12 @@ package com.devsuperior.dslit.services;
 
 import com.devsuperior.dslit.dto.GameListResponseDTO;
 import com.devsuperior.dslit.entities.GameList;
+import com.devsuperior.dslit.projections.GameMinProjection;
 import com.devsuperior.dslit.repositories.GameListRepository;
+import com.devsuperior.dslit.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,6 +18,9 @@ public class GameListService {
     @Autowired
     private GameListRepository repository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     public GameListResponseDTO findById(Long id) {
         GameList res = repository.findById(id).orElseThrow(() -> new RuntimeException("Game List not found"));
         return new GameListResponseDTO(res);
@@ -23,5 +29,21 @@ public class GameListService {
     public List<GameListResponseDTO> findAll() {
         List<GameList> res = repository.findAll();
         return res.stream().map(GameListResponseDTO::new).toList();
+    }
+
+    @Transactional
+    public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+        List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+        GameMinProjection obj = list.remove(sourceIndex);
+        list.add(destinationIndex, obj);
+
+        int min = Math.min(sourceIndex, destinationIndex);
+        int max = Math.max(sourceIndex, destinationIndex);
+
+        for (int i = min; i <= max; i++) {
+            repository.updateBelongingPosition(listId, list.get(i).getId(), i);
+        }
     }
 }
